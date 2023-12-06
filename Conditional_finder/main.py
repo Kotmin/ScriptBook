0d70d4a344a1eb1c81384efdf900488ff9f930d3
kotmin <kotmin@users.noreply.github.com>
2023-12-06 23:19:43 +0100

    innit of job searcher

---
+++ Conditional_finder/main.py
@@ -0,0 +1,15 @@
+
+
+
+URL = "https://justjoin.it/employment-type_internship.permanent/experience-level_junior.mid/remote_yes/salary_500.100000"
+
+ URLs = ["https://justjoin.it/employment-type_internship.permanent/experience-level_junior.mid/remote_yes/salary_500.100000",
+            # just Python
+            # "https://justjoin.it/employment-type_internship.permanent/experience-level_junior.mid/remote_yes/salary_500.100000?keyword=python",
+            "https://justjoin.it/all-locations/python/employment-type_internship.permanent/experience-level_junior.mid/remote_yes/salary_500.100000",
+            
+            ]
+
+
+
+already_covered_companies ["Sigma It", "Reply Polska"]
